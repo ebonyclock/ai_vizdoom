b27f31c1c59e763d4ff3099253ca5dd718db8ef9
michal <kempk.michal@gmail.com>
2016-06-16 23:30:50 +0200

    Some changes in slurm script.

--- run_on_slurm.sh
+++ run_on_slurm.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-instances=5
-for i in  $(seq 0 $((instances-1))) ; do
-    name=health_baseline_$i
-    logfile=logs/$name
-    export THEANO_FLAGS=base_compiledir=.theano_stuff/$i
-    srun  -x ./excluded_hosts -p lab-ci --constraint=cuda --exclusive  python -u learn.py health_baseline -n $name --no-tqdm -e 25 > $logfile  &
+for i in  0 1 2 3 4 ; do
+    name="health_baseline_duel_${i}"
+    jobname="hb4_${i}"
+    logfile="logs/${name}"
+    export THEANO_FLAGS=base_compiledir=".theano_stuff/duel_${i}"
+    srun  -J "$jobname" -x ./excluded_hosts -p lab-ci --constraint=cuda --exclusive  python -u learn.py health_baseline -n "$name" --no-save --no-save-best --no-tqdm -e 25 > "$logfile"  &
 done
 
